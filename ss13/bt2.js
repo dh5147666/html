b662a065ba01d5c00cf60fb277f90139a2cd6e36
dh5147666 <dh5147666@gmail.com>
2025-02-26 16:01:45 +0700

    add

---
+++ ss13/bt2.js
@@ -0,0 +1,12 @@
+// Khai báo biến với các kiểu dữ liệu khác nhau
+var number = 10;       // Số nguyên
+var price = 20.5;      // Số thực
+var status = true;     // Kiểu logic (Boolean)
+var city = "Hà Nội";   // Kiểu chuỗi (String)
+
+// In kết quả ra màn hình bằng document.write()
+document.write("<h2>Kết quả hiển thị:</h2>");
+document.write("<p>Biến number (số nguyên): " + number + "</p>");
+document.write("<p>Biến price (số thực): " + price + "</p>");
+document.write("<p>Biến status (logic): " + status + "</p>");
+document.write("<p>Biến city (chuỗi): " + city + "</p>");
---
+++ ss13/bt3.js
@@ -0,0 +1,8 @@
+
+console.log("Welcome to Rikkei Academy");
+
+
+document.write("<h2>Quyết tâm học lập trình JavaScript</h2>");
+
+
+alert("Chào mừng bạn đến RIKKEI");
